66e52a0706b221c654c7ce3ce3f4c45d94b6aabf
Krzysztof Czechowski <krzysztof.czechowski@b-mind.pl>
2019-02-05 18:19:43 +0100

    Phpdoc

--- app/Connection.php
+++ app/Connection.php
@@ -4,15 +4,26 @@ namespace App;
 
 use React\Socket\ConnectionInterface;
 
+/**
+ * Class Connection
+ * @package App
+ */
 class Connection
 {
 
+    /** @var \React\Socket\ConnectionInterface */
     private $rawConnection;
 
+    /** @var string */
     private $localAddress;
 
+    /** @var string */
     private $remoteAddress;
 
+    /**
+     * Connection constructor.
+     * @param \React\Socket\ConnectionInterface $connection
+     */
     public function __construct(ConnectionInterface &$connection)
     {
         $this->rawConnection = $connection;
@@ -22,18 +33,21 @@ class Connection
         $this->registerConnectionEvents();
     }
 
+    /**
+     * Register handlers for all connection events
+     */
     protected function registerConnectionEvents()
     {
         $this->rawConnection->on('data', $this->onConnectionData());
-
         $this->rawConnection->on('close', $this->onConnectionClose());
-
         $this->rawConnection->on('end', $this->onConnectionEnd());
-
         $this->rawConnection->on('error', $this->onConnectionError());
     }
 
-    protected function onConnectionData()
+    /**
+     * @return \Closure
+     */
+    protected function onConnectionData(): \Closure
     {
         return function ($data) {
             $data = trim($data);
@@ -41,21 +55,30 @@ class Connection
         };
     }
 
-    protected function onConnectionClose()
+    /**
+     * @return \Closure
+     */
+    protected function onConnectionClose(): \Closure
     {
         return function () {
             $this->echo("Connection closed", 'x-');
         };
     }
 
-    protected function onConnectionEnd()
+    /**
+     * @return \Closure
+     */
+    protected function onConnectionEnd(): \Closure
     {
         return function () {
             $this->echo("Connection ended", '-x');
         };
     }
 
-    protected function onConnectionError()
+    /**
+     * @return \Closure
+     */
+    protected function onConnectionError(): \Closure
     {
         return function () {
             $this->echo("Connection error", '!!');
